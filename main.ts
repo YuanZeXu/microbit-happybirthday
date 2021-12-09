b57bd283d5b32b1d487d0c5b49453ca21c5b9f8c
YuanZeXu <jeffrey0420@gmail.com>
2021-12-09 16:01:31 +0800

    Update LED and add variable

--- main.ts
+++ main.ts
@@ -1,25 +1,38 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendString("HBD")
+    StopAll = 1
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendString("RUN")
+    StopAll = 2
 })
 radio.onReceivedString(function (receivedString) {
     music.stopAllSounds()
+    StopAll = 0
     if (receivedString == "HBD") {
+        StopAll = 1
         music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Forever)
     } else if (receivedString == "RUN") {
+        StopAll = 2
         music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Forever)
     }
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendString("Stop")
+    StopAll = 0
 })
+let StopAll = 0
 radio.setGroup(56)
 basic.forever(function () {
-    basic.showIcon(IconNames.SmallHeart)
-    basic.showIcon(IconNames.Heart)
-    basic.showString("HAPPY BIRTHDAY TO YOU")
-    basic.showIcon(IconNames.SmallHeart)
-    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+    if (StopAll == 1) {
+        basic.showIcon(IconNames.SmallHeart)
+        basic.showIcon(IconNames.Heart)
+        basic.showString("HBD TO STONE")
+        basic.pause(1000)
+    } else if (StopAll == 2) {
+        basic.showIcon(IconNames.Rollerskate)
+        basic.showIcon(IconNames.Duck)
+        basic.showIcon(IconNames.Giraffe)
+    }
 })
